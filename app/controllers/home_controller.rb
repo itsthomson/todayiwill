f62c55db23a3c02805d6ee486fd2f1dfcaf6bab4
Jimmy Kittiyachavalit <jimmy@jib.(none)>
2010-04-06 17:55:39 +0000

    Move logic into controller and clean up view.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
   def index
      @posts = Post.all
 
+     num = rand(@posts.length)
+     @featured_post = @posts[num]
+
       respond_to do |format|
         format.html # index.html.erb
         format.xml  { render :xml => @posts }
